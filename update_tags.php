bf53548034faf93432ab784398e70fbe5e300ece
chell2 <rei1259@gmail.com>
2024-10-16 08:09:38 +0900

    修正

--- update_tags.php
+++ update_tags.php
@@ -16,9 +16,6 @@ $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':tag_name', $tag_name, PDO::PARAM_STR);
 $status = $stmt->execute();
 
-// データ挿入後のIDを取得
-$inserted_id = $pdo->lastInsertId();
-
 //４．データ登録処理後
 if($status==false){
   sql_error($stmt);
